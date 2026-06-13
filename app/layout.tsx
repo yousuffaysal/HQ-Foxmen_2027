196c2bafd2a37f990e5821c5e4d2742dad382b84
YusuF Faisal <yusufoesta7t@gmail.com>
2026-06-13 23:00:12 +0600

    feat: add Google Analytics (G-J3RWYPW4EH) to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -111,6 +111,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         suppressHydrationWarning
       >
         <SiteShell>{children}</SiteShell>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-J3RWYPW4EH" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-J3RWYPW4EH');
+        `}</Script>
         <Script id="cal-embed" strategy="lazyOnload">{`
           (function(C,A,L){let p=function(a,ar){a.q.push(ar);};let d=C.document;C.Cal=C.Cal||function(){let cal=C.Cal;let ar=arguments;if(!cal.loaded){cal.ns={};cal.q=cal.q||[];d.head.appendChild(d.createElement("script")).src=A;cal.loaded=true;}if(ar[0]===L){const api=function(){p(api,arguments);};const namespace=ar[1];api.q=api.q||[];if(typeof namespace==="string"){cal.ns[namespace]=cal.ns[namespace]||api;p(cal.ns[namespace],ar);p(cal,["initNamespace",namespace]);}else p(cal,ar);return;}p(cal,ar);};})(window,"https://app.cal.com/embed/embed.js","init");
           Cal("init","project-discussion-call",{origin:"https://app.cal.com"});
